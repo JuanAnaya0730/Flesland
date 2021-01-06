42073f9ab578dfd10483dd323057d2db39f0132f
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-06 03:56:58 -0500

    Se añadio el codigo funcional.

--- charco.cpp
+++ charco.cpp
@@ -1,6 +1,19 @@
 #include "charco.h"
 
-Charco::Charco()
+Charco::Charco(int pX, int pY, int ancho_, int alto_)
 {
+    posicionX = pX;
+    posicionY = pY;
+    Ancho = ancho_;
+    Alto = alto_;
+}
 
+QRectF Charco::boundingRect() const
+{
+    return QRectF(posicionX, posicionY, Ancho, Alto);
+}
+
+void Charco::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawRect(boundingRect());
 }
--- charco.h
+++ charco.h
@@ -1,11 +1,27 @@
+/*! La clase "Charco" se encargara de ralentizar al personaje principal en caso de
+ *  que este entre en lo que graficamente sera un charco.
+*/
+
 #ifndef CHARCO_H
 #define CHARCO_H
 
+#include <QGraphicsRectItem>
+#include <QPainter>
 
-class Charco
+class Charco:
+        public QGraphicsRectItem
 {
+private:
+    int posicionX;
+    int posicionY;
+    int Ancho;
+    int Alto;
+
 public:
-    Charco();
+    Charco(int pX, int pY, int ancho_, int alto_);
+
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 };
 
 #endif // CHARCO_H
