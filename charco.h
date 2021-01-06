2a1c49a218ed13d136741f3a43cb258555474739
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-06 04:21:18 -0500

    Se comento un poco el codigo.

--- charco.h
+++ charco.h
@@ -12,12 +12,14 @@ class Charco:
         public QGraphicsRectItem
 {
 private:
-    int posicionX;
-    int posicionY;
-    int Ancho;
-    int Alto;
+    int posicionX; // Posicion en X del charco.
+    int posicionY; // Posicion en Y del charco.
+    int Ancho; // Ancho que tendra el charco.
+    int Alto; // Alto que tendra el charco.
 
 public:
+    /* El constructor debera recibir la posicion en X y Y en la que se quiere que se
+     ubique el charco asi como tambien un ancho y alto para para este. */
     Charco(int pX, int pY, int ancho_, int alto_);
 
     QRectF boundingRect() const;
