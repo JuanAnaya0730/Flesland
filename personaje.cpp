59140d3c532022b368077883a0a86fa69ceac0d0
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-28 16:38:03 -0500

    Se añadieron funciones.

--- personaje.cpp
+++ personaje.cpp
@@ -1,6 +1,95 @@
 #include "personaje.h"
+#include <QDebug>
 
-Personaje::Personaje()
+Personaje::Personaje(int posicionInicialX,int posicionInicialY)
 {
+    timer = new QTimer();
 
+    V0 = 50;
+    Vx = V0 * cos(Angulo);
+    Vy = V0 * sin(Angulo);
+
+    X0 = posicionInicialX;
+    Y0 = posicionInicialY;
+    setRect(0, 0, 80, 85);
+    setPos(X0, Y0);
+
+    posicionX = X0;
+
+    connect(timer, &QTimer::timeout, this, &Personaje::actualizarMParabolico);
+    timerOn();
+}
+
+void Personaje::actualizarMParabolico()
+{
+    //posicionX = X0 + Vx*t;
+    posicionY = Y0 - Vy*t + ((G*t*t)/2);
+    setPos(posicionX, posicionY);
+    t += 0.1;
+}
+
+void Personaje::Disparar()
+{
+    disparo = new Cuchillo(x()+40, y()+37);
+    qDebug() << &disparo;
+}
+
+void Personaje::saltar()
+{
+    Y0 = y();
+    t = 0;
+    timerOn();
+}
+
+void Personaje::bajar()
+{
+    t = 10.7;
+    timerOn();
+}
+
+void Personaje::adelantar()
+{
+    posicionX+=1;
+    setPos(posicionX, y());
+}
+
+void Personaje::timerOn()
+{
+    timer->start(5);
+}
+
+void Personaje::timerOff()
+{
+    timer->stop();
+}
+
+void Personaje::setPosicionX(float value)
+{
+    posicionX = value;
+}
+
+void Personaje::setY0(float value)
+{
+    Y0 = value;
+    setPos(x(), Y0);
+}
+
+Cuchillo *Personaje::getDisparos() const
+{
+    return disparo;
+}
+
+float Personaje::getPosicionY() const
+{
+    return posicionY;
+}
+
+float Personaje::getT() const
+{
+    return t;
+}
+
+float Personaje::getPosicionX() const
+{
+    return posicionX;
 }
--- personaje.h
+++ personaje.h
@@ -1,11 +1,49 @@
 #ifndef PERSONAJE_H
 #define PERSONAJE_H
 
+#include <QGraphicsRectItem>
+#include <cuchillo.h>
+#include <QObject>
+#include <QTimer>
+#include <math.h>
 
-class Personaje
+#define Angulo (float) 1.5708 // Equivalente de 90° en radianes.
+#define G (float) 9.81 // Valor promedio de la gravedad.
+class Personaje:
+        public QObject,
+        public QGraphicsRectItem
 {
+private:
+    Cuchillo *disparo;
+    QTimer *timer;
+    float posicionX;
+    float posicionY;
+    float X0;
+    float Y0;
+    float V0;
+    float Vx;
+    float Vy;
+    float t;
+
 public:
-    Personaje();
+    Personaje(int posicionInicialX,int posicionInicialY);
+    void Disparar();
+    void saltar();
+    void bajar();
+    void adelantar();
+    void timerOn();
+    void timerOff();
+
+    Cuchillo *getDisparos() const;
+    float getPosicionX() const;
+    float getPosicionY() const;
+    float getT() const;
+    void setY0(float value);
+    void setPosicionX(float value);
+
+public slots:
+    void actualizarMParabolico();
+
 };
 
 #endif // PERSONAJE_H
