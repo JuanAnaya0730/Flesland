dda613d1030df9032390ad7332cdaf0ba629f83d
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-28 16:35:53 -0500

    Clase flecha se actualizo un poco.

--- flecha.cpp
+++ flecha.cpp
@@ -1,12 +1,43 @@
 #include "flecha.h"
 
+Flecha::Flecha(float posicionInicialX, float posicionInicialY)
+{
+    timer = new QTimer();
+
+    V0 = 78;
+    Vx = V0 * cos(Angulo);
+    Vy = V0 * sin(Angulo);
+
+    X0 = posicionInicialX;
+    Y0 = posicionInicialY;
+    setRect(0, 0, 10, 10);
+    setPos(X0, Y0);
+
+    connect(timer, &QTimer::timeout, this, &Flecha::actulalizarMparabolico);
+    timer->start(15);
+}
+
+void Flecha::actulalizarMparabolico()
+{
+    posicionX = X0 - Vx*t;
+    posicionY = Y0 - Vy*t + ((G*t*t)/2);
+    setPos(posicionX, posicionY);
+    t += 0.1;
+}
+
+/*
 Flecha::Flecha(double x, double y, double v, double a)
 {
+    timer = new QTimer();
+
     posicion_x = x;
     posicion_y = y;
     velocidad = v;
     angulo = a;
     setPos (posicion_x,posicion_y);
+
+    connect(timer, &QTimer::timeout, this, &Flecha::actualizar_posicion);
+    timer->start(5);
 }
 
 QRectF Flecha::boundingRect() const
@@ -17,8 +48,10 @@ QRectF Flecha::boundingRect() const
 void Flecha::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     // <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
-    QPixmap imagen (":/imagen/imagenes/flecha.png");
-    painter->drawPixmap(-radio,-radio,2*radio,2*radio,imagen);
+    //QPixmap imagen (":/imagen/imagenes/flecha.png");
+    //painter->drawPixmap(-radio,-radio,2*radio,2*radio,imagen);
+    painter->setBrush(Qt::blue);
+    painter->drawEllipse(boundingRect());
 }
 
 void Flecha::calcularVelocidad()
@@ -32,8 +65,8 @@ void Flecha::calcularVelocidad()
 void Flecha::actualizar_posicion()
 {
     calcularVelocidad();
-    posicion_x = posicion_x+velocidad_x *delta;
-    posicion_y = posicion_y + velocidad_y*delta-(0.5*gravedad*delta*delta);
+    posicion_x = posicion_x-velocidad_x *delta;
+    posicion_y = posicion_y - velocidad_y*delta+(0.5*gravedad*delta*delta);
     setPos (posicion_x,posicion_y);
 }
 
@@ -56,3 +89,4 @@ void Flecha::setPosicion_y(double value)
 {
     posicion_y = value;
 }
+*/
