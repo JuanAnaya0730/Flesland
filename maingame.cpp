8dbbaefa3fffdd4160ff7a21215a4f69b70760cb
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-28 16:37:28 -0500

    clase encargada de gestionar el juego.

--- maingame.cpp
+++ maingame.cpp
@@ -1,6 +1,61 @@
 #include "maingame.h"
+#include <QDebug>
 
 MainGame::MainGame()
 {
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800, 602);
 
+    scene = new QGraphicsScene();
+    timerView = new QTimer();
+    nivel1 = new Nivel();
+
+    scene->setSceneRect(0,0,10000,600);
+    setScene(scene);
+
+    nivel1->cargarNivel();
+    nivel1->correrNivel();
+
+    scene->addPixmap(QPixmap(":/Imagenes/Prueba.png"));
+    scene->addItem(nivel1->getFlesman());
+
+    timerView->start();
+    QObject::connect(timerView, &QTimer::timeout, this, &MainGame::moverView);
+
+    show();
+}
+
+void MainGame::keyPressEvent(QKeyEvent *key)
+{
+    if(key->key() == Qt::Key_W){
+        nivel1->getFlesman()->saltar();
+        //p->saltar();
+    }
+    if(key->key() == Qt::Key_S){
+        //p->bajar();
+        nivel1->getFlesman()->bajar();
+    }
+    if(key->key() == Qt::Key_D){
+        //p->bajar();
+        nivel1->getFlesman()->adelantar();
+    }
+    if(key->key() == Qt::Key_Space){
+        nivel1->getFlesman()->Disparar();
+        scene->addItem(nivel1->getFlesman()->getDisparos());
+    }
+}
+
+void MainGame::moverView()
+{
+//    for(int i=0; i<nivel1->getPlataformas().size(); ++i){
+//        if(p->collidesWithItem(nivel1->getPlataformas().at(i)) && p->getT()>=7){
+//            if(p->getPosicionY()+85 <= nivel1->getPlataformas().at(i)->getPosy()+nivel1->getPlataformas().at(i)->getH()){
+//                p->setY0(nivel1->getPlataformas().at(i)->getPosy()-86);
+//                p->timerOff();
+//            }
+//        }
+//    }
+    //centerOn(*nivel1->getCentro());
+    centerOn(2000, 474);
 }
--- maingame.h
+++ maingame.h
@@ -1,11 +1,28 @@
 #ifndef MAINGAME_H
 #define MAINGAME_H
 
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QKeyEvent>
+#include <QObject>
+#include <QTimer>
+#include "nivel.h"
 
-class MainGame
+class MainGame:
+        public QGraphicsView
 {
+private:
+    QGraphicsScene *scene;
+    QTimer *timerView;
+    Nivel *nivel1;
+
 public:
     MainGame();
+    void keyPressEvent(QKeyEvent *key);
+
+public slots:
+    void moverView();
+
 };
 
 #endif // MAINGAME_H
