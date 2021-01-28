c73147b79bf28c2051fbcee22a57c3a287fc7aa5
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-28 16:37:11 -0500

    Se llama la clase maingame que se encargara de gestionar todo el juego.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <QApplication>
+#include "maingame.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    MainGame juego;
+    juego.show();
 
     return a.exec();
 }
