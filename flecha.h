4046decfa5006dd4d0a9a0cfe5f4987e2f14acdd
Juan Anaya <juan.anayar@udea.edu.co>
2021-01-28 16:36:12 -0500

    La clase flecha se actualizo un poco.

--- flecha.h
+++ flecha.h
@@ -1,41 +1,57 @@
 #ifndef FLECHA_H
 #define FLECHA_H
 
-#include <QGraphicsItem>
-#include <QPainter>
-#include <QPixmap>
+#include <QGraphicsRectItem>
+#include <QObject>
+#include <QTimer>
 #include <math.h>
 
+#define Angulo (float) 1.22173
+#define G (float) 9.81
+
 class Flecha:
-        public QGraphicsItem
+        public QObject,
+        public QGraphicsRectItem
 {
 private:
-    double posicion_x;
-    double posicion_y;
-    double velocidad;
-    double angulo;
-    double velocidad_x;
-    double velocidad_y;
-    const double delta = 0.01;
-    const double gravedad = 9.8;
+    QTimer *timer;
+    float posicionX;
+    float posicionY;
+    float X0;
+    float Y0;
+    float V0;
+    float Vx;
+    float Vy;
+    float t;
 
-    int ancho;
-    int alto;
-    int radio= 15;
+//    QTimer *timer;
+//    double posicion_x;
+//    double posicion_y;
+//    double velocidad;
+//    double angulo;
+//    double velocidad_x;
+//    double velocidad_y;
+//    const double delta = 0.01;
+//    const double gravedad = 9.8;
+
+//    int ancho;
+//    int alto;
+//    int radio= 15;
 
 public:
-    Flecha(double x, double y, double v, double a);
+    Flecha(float posicionInicialX, float posicionInicialY);
 
-    QRectF boundingRect() const;
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+    void actulalizarMparabolico();
+//    QRectF boundingRect() const;
+//    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
 
-    double getPosicion_x() const;
-    void setPosicion_x(double value);
-    double getPosicion_y() const;
-    void setPosicion_y(double value);
+//    double getPosicion_x() const;
+//    void setPosicion_x(double value);
+//    double getPosicion_y() const;
+//    void setPosicion_y(double value);
 
-    void actualizar_posicion ();
-    void calcularVelocidad();
+//    void actualizar_posicion ();
+//    void calcularVelocidad();
 
 };
 
